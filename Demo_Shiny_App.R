b6f52dde78265c60ba5eacb05d7442ee6bcc9388
Alfredo.G.Marquez <alfredo.g.marquez@gmail.com>
2016-12-13 23:51:42 -0600

    Demo App

---
+++ Demo_Shiny_App.R
@@ -0,0 +1,25 @@
+# Demo Shiny App
+# source: http://shiny.rstudio.com/articles/shinyapps.html
+
+home <- getwd()
+setwd('C:\\Users\\marqu\\Documents\\Shiny')
+
+library(devtools)
+library(rsconnect)
+library(shiny)
+library(ggplot2)
+
+rsconnect::setAccountInfo(name='fredoxvii',
+                          token='B402329ACE6C89A4A67D989A7685ED75',
+                          secret='aNZvdhgiqNcARzaSFUt1spgCp/3Y570qQBC2zTor')
+
+
+rsconnect::deployApp(getwd())
+
+library(shiny)
+runApp()
+
+
+terminateApp("<your app's name>")
+rsconnect::terminateApp
+setwd(home)
