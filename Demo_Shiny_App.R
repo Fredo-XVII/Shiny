314777c243bc7f9f6f7137cc13a1caaad327fe30
Alfredo.G.Marquez <alfredo.g.marquez@gmail.com>
2016-12-13 23:56:14 -0600

    closing notes

--- Demo_Shiny_App.R
+++ Demo_Shiny_App.R
@@ -13,13 +13,17 @@ rsconnect::setAccountInfo(name='fredoxvii',
                           token='B402329ACE6C89A4A67D989A7685ED75',
                           secret='aNZvdhgiqNcARzaSFUt1spgCp/3Y570qQBC2zTor')
 
-
+# Deploy the App
 rsconnect::deployApp(getwd())
-
 library(shiny)
 runApp()
 
-
+# Terminate the App
 terminateApp("<your app's name>")
 rsconnect::terminateApp
+
+# Closing 
+save.image()
 setwd(home)
+
+
